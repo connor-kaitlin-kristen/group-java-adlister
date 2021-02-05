89b4e4d7da1562847ea96fcd6b0d141ecd58d017
kcollier10 <kristencollier10@gmail.com>
2021-02-05 14:55:19 -0600

    feat: added function to allow users to edit profile

--- src/main/java/com/codeup/adlister/controllers/EditProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditProfileServlet.java
@@ -1,5 +1,9 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,6 +21,36 @@ public class EditProfileServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String newUsername = req.getParameter("newUsername");
+        String newEmail = req.getParameter("newEmail");
+        String oldPassword = req.getParameter("oldPassword");
+        String newPassword = req.getParameter("newPassword");
+        String newPasswordConfirm = req.getParameter("newPasswordConfirm");
+        User user = (User) req.getSession().getAttribute("user");
+
+        boolean inputHasErrors = newUsername.isEmpty()
+                || newEmail.isEmpty()
+                || newPassword.isEmpty()
+                || (newPassword.equals(oldPassword))
+                || (!newPassword.equals(newPasswordConfirm));
+
+        if(inputHasErrors) {
+            resp.sendRedirect("/edit");
+            return;
+        } else if (DaoFactory.getUsersDao().findByUsername(newUsername) != null) {
+            resp.sendRedirect("/edit");
+            return;
+        }
+
+        user.setUsername(newUsername);
+        user.setEmail(newEmail);
+        user.setPassword(newPassword);
+
+        DaoFactory.getUsersDao().updateProfile(user);
+        resp.sendRedirect("/profile");
+
+
 
     }
+
 }
